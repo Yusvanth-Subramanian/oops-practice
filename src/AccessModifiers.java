9e08d902f0a55cc28cd4fd69e81ef123e733060b
Yusvanth Subramanian <yusvanth@terzocloud.com>
2023-06-14 12:10:09 +0530

    Access Modifiers

---
+++ src/AccessModifiers.java
@@ -0,0 +1,21 @@
+public class AccessModifiers {
+
+
+    // Accessible Everywhere
+    public int a=5;
+
+    // Accessible to all inside the package and subclasses in other packages
+    protected int b = 10;
+
+    // Accessible only within the package
+    int c = 15;
+
+    // Only accessible within the class
+    private int d = 20;
+
+    public static void main(String args[]){
+
+
+
+    }
+}
